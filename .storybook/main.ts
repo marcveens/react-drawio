0f1ccafaeb29997f4ff7357b0548116311512a58
Marc Veens <marcveens@hotmail.com>
2024-03-12 20:06:49 +0100

    Upgraded Storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-storysource'
   ],
   framework: {
