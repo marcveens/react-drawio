8c870087cbb3409f8b767172ec328590a920df37
Marc Veens <Marc.Veens@macaw.nl>
2024-03-12 18:50:31 +0100

    Deploying Storybook as demo environment

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions'
+    '@storybook/addon-storysource'
   ],
   framework: {
     name: '@storybook/react-vite',
