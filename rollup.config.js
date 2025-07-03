13878b392cfe62dd35d7d57ae218cc86277439c7
Marc Veens <veens.marc@gmail.com>
2025-07-03 09:46:25 +0200

    fixed ts issue (#59)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { swc } from 'rollup-plugin-swc3';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import del from 'rollup-plugin-delete';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 const extensions = ['.js', '.ts', '.tsx'];
 const external = (_) => /node_modules/.test(_) && !/@swc\/helpers/.test(_);
